63cd5de221c49e676a53be6968619b6617c4dc63
Arturo Mora <luartmgr@gmail.com>
2018-10-15 06:55:25 -0600

    [ADD] Top moved to src

--- src/memory/imem.sv
+++ src/memory/imem.sv
@@ -7,14 +7,15 @@
  */
  
  
- mdoule imem(	input logic [31:0] a,
-					output logic [31:0] rd);
+module imem
+(
+     input logic [31:0] a,
+    output logic [31:0] rd);
 
-	logic [31:0] RAM [63:0];
-	
-	initial 
-		$readmem("memfile.dat",RAM);
-		
-	assign rd = RAM[a[31:2]];
+    logic [31:0] RAM [63:0];
+    
+    initial 
+        $readmem("memfile.dat", RAM);
+    assign rd = RAM[a[31:2]];
 
 endmodule
--- testbench/top_tb.sv
+++ testbench/top_tb.sv
@@ -9,7 +9,7 @@
  
  module testbench();
 	
-	logic clk_i,rst_i;
+	logic clk_i, rst_i;
 	logic [31:0] write_data_o,dat_address_o;
 	logic mem_write_o;
 	
@@ -18,11 +18,17 @@
 				mem_write_o);
 	
 	initial begin
-		rst_i <=1; #22;rst_i=0;
+        #0
+		rst_i <=1;
+        #22
+        rst_i <=0;
 	end
 	
 	always begin
-		clk_i<=1; #5;clk_i<=0 #5;
+		#5
+        clk_i<= 1;
+        #5
+        clk_i<=0;
 	end
 	
 	always @(negedge clk_i) begin
